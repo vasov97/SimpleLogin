1b4f24a34613e27d860e7fdd86e16397ce701416
vasov97 <vasovmail@gmail.com>
2023-05-11 13:15:29 +0200

    Add iphone image

--- lib/res/app_theme/app_images.dart
+++ lib/res/app_theme/app_images.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppImages{
   static final googleLogo = Image.asset('assets/google.png');
   static final facebookLogo = Image.asset('assets/facebook.png');
+  static final phoneForBackground = Image.asset('assets/iphone.png');
 }
